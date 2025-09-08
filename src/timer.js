42dbd9c7b60f70a73ef800d21366cb3cfbb3457c
Lucas Bloodstone <lucas.bloodstone@outlook.com>
2025-09-09 04:45:09 +0600

    fix: incorrect time in log message

--- src/timer.js
+++ src/timer.js
@@ -1,8 +1,7 @@
 export function now(){
-    const time = Date.now() / 1000
-    const second = parseInt(time % 60).toString().padStart(2, "0")
-    const minute = parseInt((time % (60 * 60)) / 60).toString().padStart(2, "0")
-    const hour = parseInt(time / (60 * 60 ) % 10).toString().padStart(2, "0");
+    const second = new Date().getSeconds().toString().padStart(2, "0")
+    const minute = new Date().getMinutes().toString().padStart(2, "0")
+    const hour = new Date().getHours().toString().padStart(2, "0");
     return `[${hour}:${minute}:${second}]`
 }
 
